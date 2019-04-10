4113562e4fff521a0b3c9e2b68798d6a5c03ac23
Jon Michael Corpin Alabado <jon_alabado2@comcast.net>
2019-04-09 19:47:45 -0500

    Added code to all files

---
+++ Assignment1.js
@@ -0,0 +1,10 @@
+var $myDiv = $('#div1');
+var myDiv = document.getElementById('div1');
+myDiv.setAttribute('style', 'color: blue');
+
+console.log($myDiv.css("color"));
+
+var x = 2;
+var y = 2;
+
+alert(x + y);
